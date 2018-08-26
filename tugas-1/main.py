032b8931fd5f506e60b31dc4c1ce1cfadd250f26
rizkiihza <rizki.ihza@gmail.com>
2018-08-26 20:26:54 +0700

    make barplot color same with the variable

--- tugas-1/main.py
+++ tugas-1/main.py
@@ -7,27 +7,28 @@ from math import log
 img = Image.open('iron_man.jpg')
 pixels = img.convert('RGBA').load()
 h, w = img.size
-size = 16
-DIVISOR = 256 // size
 
-r = [0 for i in range(size)]
-g = [0 for i in range(size)]
-b = [0 for i in range(size)]
-gs = [0 for i in range(size)]
+K_SIZE = 16
+K_DIVISOR = 256 // K_SIZE
+
+r = [0 for i in range(K_SIZE)]
+g = [0 for i in range(K_SIZE)]
+b = [0 for i in range(K_SIZE)]
+gs = [0 for i in range(K_SIZE)]
 
 for i in range(h):
     for j in range(w):
         pixel = pixels[i, j]
-        r[pixel[0] // DIVISOR] += 1
-        g[pixel[1] // DIVISOR] += 1
-        b[pixel[2] // DIVISOR] += 1
+        r[pixel[0] // K_DIVISOR] += 1
+        g[pixel[1] // K_DIVISOR] += 1
+        b[pixel[2] // K_DIVISOR] += 1
         gs[(pixel[0]+pixel[1]+pixel[2]) // 48] += 1
 
 # print
 def get_label(inputs):
       data = []
       for unit in inputs:
-        data.append(str(DIVISOR*unit) + '-' + str(DIVISOR*(unit+1) - 1))
+        data.append(str(K_DIVISOR*unit) + '-' + str(K_DIVISOR*(unit+1) - 1))
       return data
 
 def to_logarithm(colors):
@@ -56,10 +57,10 @@ def draw():
   index = get_label(df.index.values)
   # for unit in index:
   #     print(unit)
-  sns.barplot(y=df['r logarithm'], x=index, ax=ax1)
-  sns.barplot(y=df['g logarithm'], x=index, ax=ax2)
-  sns.barplot(y=df['b logarithm'], x=index, ax=ax3)
-  sns.barplot(y=df['gs logarithm'], x=index, ax=ax4)
+  sns.barplot(y=df['r logarithm'], x=index, color='red', ax=ax1)
+  sns.barplot(y=df['g logarithm'], x=index, color='green', ax=ax2)
+  sns.barplot(y=df['b logarithm'], x=index, color='blue', ax=ax3)
+  sns.barplot(y=df['gs logarithm'], x=index, color='grey',ax=ax4)
   plt.show()
 
 
