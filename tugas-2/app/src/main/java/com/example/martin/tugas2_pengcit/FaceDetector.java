dc25b221602abc520fbfa07f57f3e71bd0ed7ce5
dewitast <dewitast20@gmail.com>
2018-11-14 18:26:13 +0700

    New class for face recognition

---
+++ tugas-2/app/src/main/java/com/example/martin/tugas2_pengcit/FaceDetector.java
@@ -0,0 +1,5 @@
+package com.example.martin.tugas2_pengcit;
+
+public class FaceDetector {
+
+}
--- tugas-2/app/src/main/java/com/example/martin/tugas2_pengcit/FaceRecognition.java
+++ tugas-2/app/src/main/java/com/example/martin/tugas2_pengcit/FaceRecognition.java
@@ -1,9 +1,11 @@
 package com.example.martin.tugas2_pengcit;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -26,10 +28,30 @@ public class FaceRecognition extends AppCompatActivity {
         curBitmap = rawBitmap;
 
         // initiate android element variable
-        faceImageView = (ImageView) findViewById(R.id.faceImageView);
-        searchButton = (Button) findViewById(R.id.faceSearchButton);
-        backButton = (Button) findViewById(R.id.faceBackButton);
+        faceImageView = findViewById(R.id.faceImageView);
+        searchButton = findViewById(R.id.faceSearchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFace();
+            }
+        });
 
-        
+        // setup back button
+        backButton = findViewById(R.id.faceBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resIntent = new Intent();
+                resIntent.putExtra("Image", curBitmap);
+                setResult(Activity.RESULT_OK, resIntent);
+                finish();
+            }
+        });
+
+    }
+
+    void getFace() {
+
     }
 }
