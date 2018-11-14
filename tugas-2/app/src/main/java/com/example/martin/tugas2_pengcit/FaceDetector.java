bbdb938e39c4898f890b489207c3ba919ec7db6d
dewitast <dewitast20@gmail.com>
2018-11-14 18:37:01 +0700

    convert rgb to hsv

--- tugas-2/app/src/main/java/com/example/martin/tugas2_pengcit/FaceDetector.java
+++ tugas-2/app/src/main/java/com/example/martin/tugas2_pengcit/FaceDetector.java
@@ -1,5 +1,23 @@
 package com.example.martin.tugas2_pengcit;
 
-public class FaceDetector {
+import android.graphics.Color;
 
+public class FaceDetector {
+    float[][] getHSV(int[][] r, int[][] g, int[][] b, int w, int h, int index) {
+        float[][] result = new float[w][h];
+        float[] hsv = new float[3];
+
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                Color.RGBToHSV(r[i][j], g[i][j], b[i][j], hsv);
+                result[i][j] = hsv[index];
+            }
+        }
+
+        return result;
+    }
+
+    int getSkin(int[][] r, int[][] b, int[][] b, int w, int h) {
+
+    }
 }
