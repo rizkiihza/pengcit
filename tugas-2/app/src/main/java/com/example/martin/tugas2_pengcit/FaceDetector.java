687b8a81cbff7a6b532474aea8a9173cdb86410d
rizkiihza <rizki.ihza@gmail.com>
2018-11-14 18:36:32 +0700

    yCbCr

--- tugas-2/app/src/main/java/com/example/martin/tugas2_pengcit/FaceDetector.java
+++ tugas-2/app/src/main/java/com/example/martin/tugas2_pengcit/FaceDetector.java
@@ -1,5 +1,47 @@
 package com.example.martin.tugas2_pengcit;
 
 public class FaceDetector {
+    public double[][] getY(int[][] r, int[][]g, int[][] b) {
+        int row = r.length;
+        int col = r[0].length;
+        double[][] y = new double[row][col];
 
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                y[i][j] = 0.2999*r[i][j] + 0.287*g[i][j] + 0.11*b[i][j];
+            }
+        }
+
+        return y;
+    }
+
+    public double[][] getCr(int[][] r, double[][] y) {
+        int row = r.length;
+        int col = r[0].length;
+
+        double[][] result = new double[row][col];
+
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                result[i][j] = r[i][j] - y[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public double[][] getCb(int[][] b, double[][] y) {
+        int row = b.length;
+        int col = b[0].length;
+
+        double[][] result = new double[row][col];
+
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                result[i][j] = b[i][j] - y[i][j];
+            }
+        }
+
+        return result;
+    }
 }
